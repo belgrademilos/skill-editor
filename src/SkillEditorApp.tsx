718c3db23822c3cdfe434d958a476ff1f504f413
Milos Milovanovic <belgrademilosk@gmail.com>
2026-04-19 11:39:08 -0700

    dirty disclaimer removed

--- src/SkillEditorApp.tsx
+++ src/SkillEditorApp.tsx
@@ -1,11 +1,8 @@
 import { useEffect } from 'react';
 import { useSkillStore } from './store/skillStore';
 import { EditorLayout } from './components/EditorLayout';
-import { useBeforeUnload } from './hooks/useBeforeUnload';
 
 export function SkillEditorApp() {
-  useBeforeUnload();
-
   useEffect(() => {
     void useSkillStore.getState().restoreSession();
   }, []);
